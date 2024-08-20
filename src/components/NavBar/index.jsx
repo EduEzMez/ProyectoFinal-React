3d8e8e072cc984226968c439bbdca9bbb19ed9a8
EzePc2 <edu.ezequielgomez@gmail.com>
2024-08-20 15:43:44 -0300

    Ultimos Ajustes

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -21,7 +21,6 @@ const NavBar = () =>{
                     <a href="index.html"><Button texto="TIENDA"></Button></a>
                     <a href="index.html"><Button texto="AYUDA"></Button></a>
                     <CartWidget></CartWidget>
-                    <a href="index.html"><Button texto="AYUDA"></Button></a>
                 </div>
             </section>
         </section>
