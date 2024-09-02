83f79ce13c899484f293e3247e47ea7cad5a5e28
EzePc2 <edu.ezequielgomez@gmail.com>
2024-09-02 15:09:51 -0300

    Agregado Toasty

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ItemCount(){
     const [cantidad, setCantidad] = useState(0);
-
     const aumentarProducto = () => {
         setCantidad(cantidad + 1)
     }
-
     const descontarProducto = () => {
         if (cantidad > 0){
             setCantidad(cantidad - 1)
         }
     }
-
     const agregarCarrito = () => {
-        console.log("Cantidad agregada:", {cantidad})
+        console.log("Cantidad agregada:",{cantidad})
     }
-    
+
+
+    const notify = () => toast('Agregado al carrito!', {
+        position: "top-center",
+        autoClose: 800,
+        pauseOnHover: true,
+        progress: undefined,
+        theme: "dark",
+        });;
+
     return (
         <div>
             <p>{cantidad}</p>
             <button onClick={aumentarProducto}>+</button>
             <button onClick={descontarProducto}>-</button>
-            <button onClick={agregarCarrito}>Agregar</button>
+            <button onClick={() => {agregarCarrito(); notify()}}>Agregar</button>
+            <ToastContainer></ToastContainer>
         </div>
     )
-
 }
-
 export default ItemCount;
