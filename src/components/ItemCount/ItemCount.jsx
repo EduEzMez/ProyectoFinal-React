6a438ced057a3b2f41371e02186ac3c7c2c079aa
Eduardo Ezequiel Gomez <edu.ezequielgomez@gmail.com>
2024-09-02 20:52:51 -0300

    add

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -2,15 +2,14 @@ import { useState } from "react";
 
 function ItemCount(){
     const [counter, setCounter] = useState(0);
-    
+
     return (
         <div>
             <p>{counter}</p>
-            <button onClick={() => setCounter(counter+1)}>+</button>
+            <button onClick={() => setCounter(counter + 1)}>+</button>
             <button onClick={() => setCounter(counter - 1)}>-</button>
         </div>
     )
-
 }
 
 export default ItemCount;
