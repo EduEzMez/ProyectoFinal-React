e18c89f65aca205b4ac478b59480bcbd3de26568
NotebookEze <edu.ezequielgomez@gmail.com>
2024-09-04 20:24:47 -0300

    Build

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import NavBar from './components/NavBar'
 import ItemListContainer from "./components/ItemListContainer";
-import Card from './components/Card/Card';
 import ItemList from './components/Items/Card';
 import './App.css'
 
